dffdbea0761bea94e8a397aaf407568f4b682243
Victor Oni <seunoni34@gmail.com>
2022-04-24 18:30:03 +0200

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HobbiesModule } from './hobbies/hobbies.module';
+import { UsersModule } from './users/users.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }),
+      inject: [ConfigService],
+    }),
+    HobbiesModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
